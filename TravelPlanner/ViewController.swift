bf60abc0b254f84d95973e2cea33c02fcfb055ed
Bruce Burgess <bruceburgess67@yahoo.com>
2016-03-06 09:49:05 +0700

    added a UIPickerView and Switch

--- TravelPlanner/ViewController.swift
+++ TravelPlanner/ViewController.swift
@@ -8,16 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    //Connect the picker view to code
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var mySwitch: UISwitch!
+    
+    let vacationOptions : [String] = ["Beach", "Mountains"]
+    
+    var selectedOption : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.pickerView.dataSource = self
+        self.pickerView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Sets the number of rows in the picker view
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.vacationOptions.count
+    }
+    //sets the columns
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    //Setting the text in the picker view
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.vacationOptions[row]
+    }
+    //Getting the selection from the picker view
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.selectedOption = self.vacationOptions[row]
     }
 
 
